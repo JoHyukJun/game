7f1ccccabd686f3645468110615e4517a4b068d8
42decibel <hyukzuny@gmail.com>
2023-09-01 22:24:10 +0900

    feat: Test swiftui

--- src/nfo-gen/ContentView.swift
+++ src/nfo-gen/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var test: String = ""
+    
+    @State private var tableData = [TestData(t1: "a",
+                                            t2: "b",
+                                            t3: "c"),
+                                   TestData(t1: "d",
+                                            t2: "e",
+                                            t3: "f")]
+    
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("nfo-test")
+            
+            Text("nfo-gen")
+            
+            TextField("input str", text: $test)
+                .padding()
+                .background(.thinMaterial)
+            
+            Text("TEST \(test)")
         }
         .padding()
+        
+        Table(tableData) {
+            TableColumn("t1") {
+                Text($0.t1)
+            }
+            TableColumn("t2") {
+                Text($0.t2)
+            }
+            TableColumn("t3") {
+                Text($0.t3 ?? "")
+            }
+        }
     }
 }
 
--- src/nfo-gen/nfo_genApp.swift
+++ src/nfo-gen/nfo_genApp.swift
@@ -12,6 +12,9 @@ struct nfo_genApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .frame(minWidth: 720, idealWidth: 1920, maxWidth: 3840,
+                       minHeight: 480, idealHeight: 1080, maxHeight: 2160,
+                       alignment: .center)
         }
     }
 }
