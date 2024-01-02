367f03b4e2395727c48376f81bde467068427757
42decibel <hyukzuny@gmail.com>
2024-01-02 19:18:25 +0900

    feat: Add prj src

---
+++ unity/Cigar Flight/Assets/Scripts/Weapon.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 10f;
+
+    [SerializeField]
+    public float damage = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.up * moveSpeed * Time.deltaTime;
+        
+    }
+}
